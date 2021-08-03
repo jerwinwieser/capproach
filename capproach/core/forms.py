03718889d67efc54dcab52ddecf1c38ed5366c09
Jerwin Wieser <jerwinwieser@macbook.local>
2021-08-03 18:37:55 +0200

    install bootstrapmodals

--- capproach/core/forms.py
+++ capproach/core/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from bootstrap_modal_forms.forms import BSModalModelForm
-from base.models import Contact
+from core.models import Contact
 
 class ContactForm(BSModalModelForm):
 	class Meta:
